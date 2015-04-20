13bcb93c3e6b69da21393cd945c6318d43b521f5
DripDry <Nizi2734@colorado.edu>
2015-04-19 23:48:00 -0600

    Updated Deck.h

--- Deck.h
+++ Deck.h
@@ -5,26 +5,33 @@
 
 #ifndef DECK_H
 #define DECK_H
-
 #include <iostream>
 #include <vector>
 
-struct card
-{
-    int card;
-    std::string suit;
+struct Card{
+	std::string suit;	//Club first, Diamond second, Heart third, Spade last -- ORDER MATTERS
+	int rank;
+	
+	Card(){}
+	
+	Card(std::string s, int r){
+		suit = s;
+		rank = r;
+	}
 };
-
 
 class Deck
 {
-    public:
-        Deck();
-        virtual ~Deck();
-        void resetDeck();
-    protected:
-    private:
-        std::vector<card> deck;
+	public:
+		Deck();
+		~Deck();
+		void printCard(Card);
+		void printDeck();
+		void resetDeck();
+		void shuffle();
+		void deal(int);
+	private:
+		std::vector<Card> deck;
 };
 
-#endif // DECK_H
+#endif
