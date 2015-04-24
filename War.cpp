dba663bcd2cb57c5a0b5572979386f1972b481ff
OmniBro <jake.mitchell@colorado.edu>
2015-04-24 16:26:54 -0600

    Adding the War header and cpp file, no functions implemented ye

---
+++ War.cpp
@@ -0,0 +1,8 @@
+/*
+  Nicholas Zimmerer
+  Jake Mitchell
+  Hoenigman CSCI2270
+  4-24-15
+*/
+
+#include "War.h"
---
+++ War.h
@@ -0,0 +1,24 @@
+/*
+  Nicholas Zimmerer
+  Jake Mitchell
+  Hoenigman CSCI2270
+  4-24-15
+*/
+
+#IFNDEF WAR_H
+#DEFINE WAR_H
+#include <iostream>
+#include <vector>
+#inlcude "Deck.h"
+
+class War::Deck
+{
+    public:
+        War();
+        ~War();
+    private:
+};
+
+
+
+#endif
